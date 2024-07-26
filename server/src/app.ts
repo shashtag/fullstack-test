108479f48902d40256587bf1eae32a058c6f80c8
Shashwat Gupta <shashwatsatna@gmail.com>
2024-07-26 14:18:24 +0530

    setup rate limiting

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,18 @@
 import express from "express";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  max: 5, // limit each IP to 10 requests per windowMs
+  message:
+    "Oops! It seems you’re making too many requests in a short time. Please wait a moment and try again later.", // message to send
+});
+
 app.use(cors());
+app.use(limiter);
 app.use(express.json());
 
 app.use("*", (_: express.Request, res: express.Response) =>
