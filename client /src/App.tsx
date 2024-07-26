4e7850ca1fb50f611d74ff15cce57a1305a38164
Shashwat Gupta <shashwatsatna@gmail.com>
2024-07-26 18:52:39 +0530

    add input and preview

--- client /src/App.tsx
+++ client /src/App.tsx
@@ -1,5 +1,60 @@
+import { useState } from "react";
+
 function App() {
-  return <>ss</>;
+  const [image, setImage] = useState<File | null>(null);
+  const [previewUrl, setPreviewUrl] = useState<string | ArrayBuffer | null>(
+    null,
+  );
+  return (
+    <>
+      <input
+        type='file'
+        accept='image/*'
+        name='Image'
+        id='Image'
+        style={{ display: "none" }}
+        onChange={(e) => {
+          setImage(e.target.files![0]);
+          const reader = new FileReader();
+
+          reader.onloadend = () => {
+            setPreviewUrl(reader.result);
+          };
+
+          reader.readAsDataURL(e.target.files![0]);
+        }}
+      />
+      <label
+        htmlFor='Image'
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          background: "#99A8B7",
+          height: "20vh",
+          fontSize: "1.5rem",
+          cursor: "pointer",
+        }}>
+        Upload Image
+      </label>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr",
+          height: "80vh",
+        }}>
+        <div
+          style={{
+            backgroundColor: "#7B937F",
+            backgroundImage: `url(${previewUrl})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "contain",
+          }}></div>
+        <textarea />
+      </div>
+    </>
+  );
 }
 
 export default App;
