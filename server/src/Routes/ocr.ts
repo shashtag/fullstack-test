6ab08bc8ef848c50c2d5261ed4d8dfc5aca5c35f
Shashwat Gupta <shashwatsatna@gmail.com>
2024-07-26 15:02:02 +0530

    setup tsconfig

--- server/src/Routes/ocr.ts
+++ server/src/Routes/ocr.ts
@@ -4,3 +4,5 @@ import ocrController from "../Controllers/ocr";
 const router = Router();
 
 router.post("/", ocrController.recognizeText);
+
+export default router;
--- server/src/app.ts
+++ server/src/app.ts
@@ -2,20 +2,23 @@ import express from "express";
 import cors from "cors";
 import rateLimit from "express-rate-limit";
 import helmet from "helmet";
+import ocrRouter from "./Routes/ocr";
 
 const app = express();
 
-const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minutes
-  max: 5, // limit each IP to 10 requests per windowMs
-  message:
-    "Oops! It seems you’re making too many requests in a short time. Please wait a moment and try again later.", // message to send
-});
-
 app.use(cors());
 app.use(helmet());
-app.use(limiter);
 app.use(express.json());
+app.use(
+  rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minutes
+    max: 5, // limit each IP to 10 requests per windowMs
+    message:
+      "Oops! It seems you’re making too many requests in a short time. Please wait a moment and try again later.", // message to send
+  }),
+);
+
+app.use(ocrRouter);
 
 app.use("*", (_: express.Request, res: express.Response) =>
   res.status(404).json({ error: "Route not found" }),
